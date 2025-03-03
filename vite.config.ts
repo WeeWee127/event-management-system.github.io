f04044925381ab9407b932f40eb82cc1c672b763
WeeWee127 <@email.com>
2025-03-03 13:31:24 +0200

    Restore original configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/event-management-system.github.io/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
+  base: '/event-management-system.github.io/'
 });

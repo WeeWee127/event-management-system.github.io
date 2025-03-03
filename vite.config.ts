7852cd402f41e1898e05b074ca49bd999234187b
WeeWee127 <@email.com>
2025-03-03 13:03:25 +0200

    Simplify configuration and fix paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   base: '/event-management-system.github.io/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 });

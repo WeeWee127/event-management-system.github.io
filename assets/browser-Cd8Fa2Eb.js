bcc324728e16ff4a64bff45a1d204be99851f19b
WeeWee127 <@email.com>
2025-03-09 20:32:41 +0200

    Updates

---
+++ assets/browser-Cd8Fa2Eb.js
@@ -0,0 +1,2 @@
+import{g as e}from"./index-byHpqB9c.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
+//# sourceMappingURL=browser-Cd8Fa2Eb.js.map
